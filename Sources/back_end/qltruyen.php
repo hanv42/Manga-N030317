1758c21311f6f892e411595ed98a9d386c887c92
hanv42 <45027602+hanv42@users.noreply.github.com>
2018-12-21 01:03:02 +0700

    delete

---
+++ Sources/back_end/qltruyen.php
@@ -0,0 +1,132 @@
+<?php
+ob_start();
+session_start();
+include_once('../cauhinh/ketnoi.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Dashboard | DLC shop</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+<script src="js/lumino.glyphs.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">DLC Admin</a>
+				<ul class="user-menu">
+					<li class="dropdown pull-right">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> User <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+							
+		</div><!-- /.container-fluid -->
+	</nav>
+		
+	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<ul class="nav menu">
+			<li role="presentation" class="divider"></li>
+			<li><a href="index.php"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Trang chủ</a></li>
+			<li class="active"><a href="qlsanpham.php"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> Truyện</a></li>
+			<li><a href="qldanhmuc.php"><svg class="glyph stroked notepad "><use xlink:href="#stroked-notepad"/></svg> Danh mục</a></li>
+			<li role="presentation" class="divider"></li>
+		</ul>
+		
+	</div><!--/.sidebar-->
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Truyện</h1>
+			</div>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-xs-12 col-md-12 col-lg-12">
+				<?php
+					$sql="SELECT * FROM manga join danhmucmanga On manga.id_dm = danhmucmanga.id_dm";
+					$query = mysqli_query($link, $sql);
+				?>
+				<div class="panel panel-primary">
+					<div class="panel-heading">Danh sách truyện</div>
+					<div class="panel-body">
+						<div class="bootstrap-table">
+							<div class="table-responsive">
+								<a href="#" class="btn btn-primary">Thêm truyện</a>
+								<table class="table table-bordered" style="margin-top:20px;">				
+									<thead>
+										<tr class="bg-primary">
+											<th>ID</th>
+											<th width="30%">Tên truyện</th>
+											<th width="20%">Ảnh demo</th>
+											<th>Danh mục</th>
+											<th>Tùy chọn</th>
+										</tr>
+									</thead>
+								
+									<tbody>
+										<?php
+										while ($row=mysqli_fetch_array($query)) {
+										?>
+										<tr>
+											<td><?php echo $row['id_m'];?></td>
+											<td><?php echo $row['ten_m'];?></td>
+											<td>
+												<img src="../front_end/images/MangaNoiBat/<?php echo $row['anh_m']; ?>" class="img-thumbnail">
+											</td>
+											<td><?php echo $row['ten_dm'];?></td>
+											<td>
+												<a href="#" class="btn btn-warning"><i class="fa fa-pencil" aria-hidden="true"></i> Sửa</a>
+												<a href="#" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i> Xóa</a>
+											</td>
+										</tr>
+										<?php
+										}
+										?>
+										</tbody>
+										
+								</table>							
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</div><!--/.row-->
+	</div>	<!--/.main-->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+		$('#calendar').datepicker({
+		});
+		!function ($) {
+		    $(document).on("click","ul.nav li.parent > a > span.icon", function(){          
+		        $(this).find('em:first').toggleClass("glyphicon-minus");      
+		    }); 
+		    $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+		}(window.jQuery);
+
+		$(window).on('resize', function () {
+		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+		})
+		$(window).on('resize', function () {
+		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+		})
+	</script>	
+</body>
+
+</html>
---
+++ Sources/back_end/themthanhvien.php
@@ -0,0 +1,80 @@
+ <?php
+ session_start(); 
+ ?>
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Untitled Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <style>
+    #navbar{
+       margin-top:50px;}
+       #tbl-first-row{
+           font-weight:bold;}	
+           #logout{
+               padding-right:30px;}	
+           </style>
+       </head>
+       <body>
+
+        <div class="container">
+            <div id="navbar" class="row">
+               <div class="col-sm-12">
+                   <nav class="navbar navbar-default">
+                      <div class="container-fluid">
+                       <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="index.php">Users</a></li>
+                        <li><a href="themthanhvien.php">Add user</a></li>
+                    </ul>
+                    <p id="logout" class="navbar-text navbar-right"><a class="navbar-link" href="#">Logout</a></p>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+       <div class="col-sm-6">
+         <?php
+         require_once("../cauhinh/ketnoi.php");
+         if (isset($_POST["submit-t"])) {
+        //lấy thông tin từ các form bằng phương thức POST
+            $email = $_POST["mail"];
+            $password = $_POST["pass"];
+
+            if ($email == "" || $password == "") {
+                echo "Bạn cần điền đầy đủ thông tin !";
+            }else{
+            // Viết câu lệnh thêm thông tin người dùng
+                $sql = "INSERT INTO admin (email, matkhau) VALUES ('$email','$password')";
+            // thực thi câu $sql với biến $link lấy từ file ketnoi.php
+                $result = mysqli_query($link,$sql);
+                if (!$result) {
+                    echo "Người dùng đã tồn tại vui lòng không trùng username và email !";
+                }else{
+                    header('Location: quanlythanhvien.php');
+                }
+
+            }
+
+        }
+        ?>
+        <form method="post" action="themthanhvien.php">
+           <div class="form-group">
+            <label>Email</label>
+            <input type="text" name="mail" class="form-control" placeholder="Email">
+        </div>
+
+        <div class="form-group">
+         <label>Password</label>
+         <input type="password" name="pass" class="form-control" placeholder="Password">
+     </div>
+     <input type="submit" name="submit-t" value="Thêm" class="btn btn-primary" />
+ </form>
+</div>
+</div>
+</div>
+</body>
+</html>
